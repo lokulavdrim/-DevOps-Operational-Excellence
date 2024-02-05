bf30c4c6468b666e61e027b76f41a4433259c880
lokulavdrim <155152348+lokulavdrim@users.noreply.github.com>
2024-02-05 10:05:49 +0100

    availability zone

--- assigment/vpc/main.tf
+++ assigment/vpc/main.tf
@@ -15,7 +15,7 @@ resource "aws_subnet" "private_subnet" {
 resource "aws_subnet" "public_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = var.public_subnet_cidr  
-  availability_zone = "us-east-1a"           
+  availability_zone = "us-east-1b"           
   map_public_ip_on_launch = true
   tags = {
     Name = "Public Subnet"
